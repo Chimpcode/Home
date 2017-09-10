b5f3478f655ecaa7604885b03e07d084cd85d35f
anargu <hello@chimpcode.cc>
2017-09-10 02:14:26 -0500

    lel

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-      }
+    export default {
+        data () {
+            return {
+            }
+        }
     }
-  }
 </script>
+
+<style>
+
+</style>
