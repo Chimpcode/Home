dc3862fb9c9986b46a079d31b76fc86e46363963
anargu <hello@chimpcode.cc>
2017-09-10 17:08:44 -0500

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
-<template lang="pug">
-    ul
-        li a
-        li b
-        li c
+<template>
+    <div class="background">
+        <section class="jumbotron">
+            <h3>We write Software</h3>
+
+            <effect-background>
+            </effect-background>
+
+        </section>
+        <div class="">
+        </div>
+    </div>
 
 </template>
 
 <script>
+    import EffectBackground from './components/EffectBackground.vue'
     export default {
+        components: {
+            EffectBackground
+        },
         data () {
             return {
 
@@ -16,7 +27,13 @@
     }
 </script>
 
-<style>
-
-
+<style lang="stylus">
+.background
+    background-color: #000
+    height: 100%
+    font-family: 'Quicksand', sans-serif;
+.jumbotron > h3
+    text-align:center
+    color: #fff
+    line-height: 600px
 </style>
